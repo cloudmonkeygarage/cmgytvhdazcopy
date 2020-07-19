ca43c1fa7a6dc92ab6d17d64a6078076335900aa
Cloud Monkey Garage <cloumonkeygarage@gmail.com>
2020-07-19 15:20:03 +0200

    Fix login and copy path

--- copyvhdwithazcopy.cmd
+++ copyvhdwithazcopy.cmd
@@ -1,6 +1,6 @@
 REM Eseguo il login alla subscription
-azcopy login --tenant-id=<tenant-id>
+azcopy login
 
 REM Eseguo la copia del disco nel blob storage su Azure
-azcopy copy 'C:\myDirectory' 'https://mystorageaccount.blob.core.windows.net/vhds' --include-path 'miodisco.vhd' 
+azcopy copy 'C:\myDirectory\miodisco.vhd' 'https://mystorageaccount.blob.core.windows.net/vhds' 
 
