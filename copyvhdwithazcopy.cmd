32e2e5000f00c1b7f25270f38d61974f6cb498ff
Cloud Monkey Garage <cloumonkeygarage@gmail.com>
2020-07-19 12:18:07 +0200

    initial release

---
+++ copyvhdwithazcopy.cmd
@@ -0,0 +1,5 @@
+REM Eseguo il login alla subscription
+azcopy login --tenant-id=<tenant-id>
+
+REM Eseguo la copia del disco nel blob storage su Azure
+azcopy copy 'C:\myDirectory' 'https://mystorageaccount.blob.core.windows.net/vhds' --include-path 'miodisco.vhd' 
